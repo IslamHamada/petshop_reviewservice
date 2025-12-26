3b98fcb2fb20f538588c55005fe7a4bd0d68402f
IslamHamada <islamhamada222@gmail.com>
2025-12-26 17:34:58 +0100

    add base review service project

---
+++ src/main/java/com/islamhamada/petshop/reviewservice/ReviewServiceBoot.java
@@ -0,0 +1,12 @@
+package com.islamhamada.petshop.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServiceBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceBoot.class, args);
+	}
+}
---
+++ src/test/java/com/islamhamada/petshop/reviewservice/ReviewServiceBootTests.java
@@ -0,0 +1,13 @@
+package com.islamhamada.petshop.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceBootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
