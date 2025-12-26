29b2a7a6a6d18e446f6f11602ecdd5cd10919431
IslamHamada <islamhamada222@gmail.com>
2025-12-26 18:07:01 +0100

    add @RequestMapping and inject ReviewService

--- src/main/java/com/islamhamada/petshop/reviewservice/controller/ReviewController.java
+++ src/main/java/com/islamhamada/petshop/reviewservice/controller/ReviewController.java
@@ -1,8 +1,14 @@
 package com.islamhamada.petshop.reviewservice.controller;
 
+import com.islamhamada.petshop.reviewservice.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/review")
 public class ReviewController {
 
+    @Autowired
+    ReviewService reviewService;
 }
