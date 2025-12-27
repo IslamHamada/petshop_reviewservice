e2ae549281e1ebe7dac5bf8123e08ba763d523f1
IslamHamada <islamhamada222@gmail.com>
2025-12-27 15:58:46 +0100

    add @Builder, @AllArgsConstructor, and @NoArgsConstructor

--- src/main/java/com/islamhamada/petshop/reviewservice/model/PostReviewRequest.java
+++ src/main/java/com/islamhamada/petshop/reviewservice/model/PostReviewRequest.java
@@ -3,9 +3,15 @@ package com.islamhamada.petshop.reviewservice.model;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostReviewRequest {
     private String text;
     @Min(1) @Max(5)
