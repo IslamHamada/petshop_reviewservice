8a4ad44ad0de005346b037bed3d60ea8b83a7275
IslamHamada <islamhamada222@gmail.com>
2025-12-27 15:57:33 +0100

    add @Builder, @NoArgsConstructor, and @AllArgsConstructor

--- src/main/java/com/islamhamada/petshop/reviewservice/exception/ReviewException.java
+++ src/main/java/com/islamhamada/petshop/reviewservice/exception/ReviewException.java
@@ -1,10 +1,16 @@
 package com.islamhamada.petshop.reviewservice.exception;
 
 import com.islamhamada.petshop.contracts.exception.ServiceException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewException extends ServiceException {
     private HttpStatus httpStatus;
 
