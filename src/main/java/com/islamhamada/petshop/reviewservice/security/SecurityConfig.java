3d72bbb950e7de6ea298ca74df2ffcfc33321341
IslamHamada <islamhamada222@gmail.com>
2025-12-29 21:46:41 +0100

    add SecurityConfig
    1- disable csrf
    2- permit all requests from outside
    3- add roles using the claim in the token

---
+++ src/main/java/com/islamhamada/petshop/reviewservice/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.islamhamada.petshop.reviewservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2ResourceServer(conf ->
+                        conf.jwt(jwt -> jwt.jwtAuthenticationConverter(converter())));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter converter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            JwtGrantedAuthoritiesConverter defaultConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> authorities = defaultConverter.convert(jwt);
+            Collection<GrantedAuthority> roles = jwt.getClaimAsStringList("https://petshop/roles")
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+            authorities.addAll(roles);
+            return authorities;
+        });
+        return converter;
+    }
+}
