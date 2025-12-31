8fe729e4a38d9920ab24972b25cb9007e43274a2
IslamHamada <islamhamada222@gmail.com>
2025-12-31 11:35:25 +0100

    add controller testing file using wiremock

---
+++ src/test/java/com/islamhamada/petshop/reviewservice/controller/ReviewControllerTest.java
@@ -0,0 +1,260 @@
+package com.islamhamada.petshop.reviewservice.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
+import com.islamhamada.petshop.reviewservice.entity.Review;
+import com.islamhamada.petshop.reviewservice.model.PostReviewRequest;
+import com.islamhamada.petshop.reviewservice.repository.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+class ReviewControllerTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper
+            = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    SimpleGrantedAuthority adminRole = new SimpleGrantedAuthority("ROLE_Admin");
+    SimpleGrantedAuthority customerRole = new SimpleGrantedAuthority("ROLE_Customer");
+
+    @BeforeEach
+    public void setup() {
+        reviewRepository.deleteAll();
+    }
+
+    @Nested
+    public class getReviewById {
+
+        SimpleGrantedAuthority neededRole = adminRole;
+        SimpleGrantedAuthority notNeededRole = customerRole;
+
+        @Test
+        public void success() throws Exception {
+            Review dbReview = reviewRepository.save(Review.builder()
+                    .text("text")
+                    .rating(3)
+                    .productId(1)
+                    .userId(2)
+                    .build());
+            long reviewId = dbReview.getId();
+            MvcResult mvcResult
+                    = mockMvc.perform(get("/review/" + reviewId)
+                    .with(jwt().authorities(neededRole))
+            ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+            String mvcResponse = mvcResult.getResponse().getContentAsString();
+            Review responseReview = objectMapper.readValue(mvcResponse, Review.class);
+            assertEquals(reviewId, responseReview.getId());
+            assertEquals(dbReview.getProductId(), responseReview.getProductId());
+            assertEquals(dbReview.getUserId(), responseReview.getUserId());
+            assertEquals(dbReview.getText(), responseReview.getText());
+            assertEquals(dbReview.getRating(), responseReview.getRating());
+        }
+
+        @Test
+        public void failure_missing_role() throws Exception {
+            mockMvc.perform(get("/review/1")
+                    .with(jwt().authorities(notNeededRole))
+            ).andExpect(MockMvcResultMatchers.status().isForbidden())
+                .andReturn();
+        }
+
+        @Test
+        public void failure_not_found() throws Exception {
+            long reviewId = 1;
+            MvcResult mvcResult = mockMvc.perform(get("/review/" + reviewId)
+                                        .with(jwt().authorities(neededRole))
+                                ).andExpect(MockMvcResultMatchers.status().isNotFound())
+                                .andReturn();
+            String mvcResponse = mvcResult.getResponse().getContentAsString();
+            RestExceptionResponse exceptionResponse = objectMapper.readValue(mvcResponse, RestExceptionResponse.class);
+            assertEquals("REVIEW_NOT_FOUND", exceptionResponse.getError_code());
+            assertEquals("Review with id: " + reviewId + " not found", exceptionResponse.getError_message());
+        }
+
+        @ParameterizedTest
+        @MethodSource("bad_inputs")
+        public void failure_bad_input(Object review_id) throws Exception {
+            MvcResult mvcResult = mockMvc.perform(get("/review/" + review_id)
+                    .with(jwt().authorities(neededRole))
+            ).andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
+
+        public static Stream<Object> bad_inputs(){
+            return Stream.of(-1, null);
+        }
+    }
+
+    @Nested
+    public class postProductReview {
+        SimpleGrantedAuthority neededRole = customerRole;
+        SimpleGrantedAuthority unneededRole = adminRole;
+
+        @Test
+        public void success() throws Exception {
+            PostReviewRequest postReviewRequest = getMockPostReviewRequest();
+            MvcResult mvcResult = mockMvc.perform(post("/review")
+                    .with(jwt().authorities(neededRole))
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(objectMapper.writeValueAsString(postReviewRequest))
+            ).andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
+
+            String mvcResponse = mvcResult.getResponse().getContentAsString();
+            Review review = objectMapper.readValue(mvcResponse, Review.class);
+
+            assertEquals(review.getProductId(), postReviewRequest.getProduct_id());
+            assertEquals(review.getUserId(), postReviewRequest.getUser_id());
+            assertEquals(review.getText(), postReviewRequest.getText());
+            assertEquals(review.getRating(), postReviewRequest.getRating());
+        }
+
+        @Test
+        public void failure_missing_role() throws Exception {
+            PostReviewRequest request = getMockPostReviewRequest();
+            mockMvc.perform(post("/review")
+                    .with(jwt().authorities(unneededRole))
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(objectMapper.writeValueAsString(request))
+            ).andExpect(MockMvcResultMatchers.status().isForbidden())
+                    .andReturn();
+        }
+
+        @ParameterizedTest
+        @MethodSource("bad_input")
+        public void failure_bad_input(PostReviewRequest request) throws Exception {
+            mockMvc.perform(post("/review")
+                    .with(jwt().authorities(neededRole))
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(objectMapper.writeValueAsString(request))
+            ).andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
+
+        public static List<PostReviewRequest> bad_input() {
+            List<PostReviewRequest> rv = new ArrayList<PostReviewRequest>();
+            rv.add(null);
+            rv.add(PostReviewRequest.builder()
+                    .product_id(-1)
+                    .user_id(2)
+                    .text("text")
+                    .rating(1)
+                    .build());
+            rv.add(PostReviewRequest.builder()
+                    .product_id(1)
+                    .user_id(-2)
+                    .text("text")
+                    .rating(1)
+                    .build());
+            rv.add(PostReviewRequest.builder()
+                    .product_id(1)
+                    .user_id(2)
+                    .text("a".repeat(256))
+                    .rating(0)
+                    .build());
+            rv.add(PostReviewRequest.builder()
+                    .product_id(1)
+                    .user_id(2)
+                    .text("text")
+                    .rating(6)
+                    .build());
+            return rv;
+        }
+
+        private PostReviewRequest getMockPostReviewRequest() {
+            PostReviewRequest postReviewRequest = PostReviewRequest.builder()
+                    .product_id(1)
+                    .user_id(2)
+                    .text("text")
+                    .rating(3)
+                    .build();
+            return postReviewRequest;
+        }
+    }
+
+    @Nested
+    public class getReviewsByProductId {
+
+        @Test
+        public void success() throws Exception {
+            long product_id = 1;
+            Review review = reviewRepository.save(
+                    Review.builder()
+                            .text("text")
+                            .rating(3)
+                            .productId(product_id)
+                            .userId(2)
+                            .build()
+            );
+            Review review2 = reviewRepository.save(
+                    Review.builder()
+                            .text("text2")
+                            .rating(1)
+                            .productId(product_id)
+                            .userId(3)
+                            .build()
+            );
+            Review review3 = reviewRepository.save(
+                    Review.builder()
+                            .text("text3")
+                            .rating(4)
+                            .productId(product_id + 1)
+                            .userId(4)
+                            .build()
+            );
+            MvcResult mvcResult = mockMvc.perform(get("/review/product/" + product_id))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
+            String response = mvcResult.getResponse().getContentAsString();
+            List<Review> productReviews = objectMapper.readValue(response, new TypeReference<List<Review>>(){});
+            assertEquals(2, productReviews.size());
+            productReviews.forEach(r -> assertEquals(product_id, r.getProductId()));
+            assertNotEquals(productReviews.get(0), productReviews.get(1));
+        }
+
+        @Test
+        public void failure_bad_input() throws Exception {
+            long product_id = -1;
+            mockMvc.perform(get("/review/product" + product_id))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
+    }
+}
