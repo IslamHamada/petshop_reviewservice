7e0a7107d8567d5cf4b0b43d09a0c97c0bd01071
IslamHamada <islamhamada222@gmail.com>
2026-01-14 20:06:10 +0100

    log ReviewException

--- src/main/java/com/islamhamada/petshop/reviewservice/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/islamhamada/petshop/reviewservice/exception/RestResponseEntityExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.islamhamada.petshop.reviewservice.exception;
 
 import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Log4j2
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ReviewException.class)
     public ResponseEntity<RestExceptionResponse> handleReviewException(ReviewException exception) {
+        log.error(exception);
         return new ResponseEntity<>(
                 RestExceptionResponse.builder()
                         .error_message(exception.getMessage())
