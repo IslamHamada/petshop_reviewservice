129f082f2fdda781799fc0f3d27c813d65c9157e
IslamHamada <islamhamada222@gmail.com>
2026-01-17 11:41:40 +0100

    sonarqube: remove unused import

--- src/test/java/com/islamhamada/petshop/reviewservice/controller/ReviewControllerTest.java
+++ src/test/java/com/islamhamada/petshop/reviewservice/controller/ReviewControllerTest.java
@@ -23,7 +23,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
